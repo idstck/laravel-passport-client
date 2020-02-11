27ed41be1540038a56a016977140f8eb56ab48a5
idstack <idstack@outlook.co.id>
2020-02-11 09:54:38 +0000

    Menggunakan Token yang Disimpan

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Timeline</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,21 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    @if (Auth::user()->token)
+                        @if ($tweets->count())
+                            @foreach ($tweets as $tweet)
+                            <div class="media mb-3">
+                                <img class="mr-3" src="https://placehold.it/64x64" alt="Generic placeholder image">
+                                <div class="media-body">
+                                    <h5 class="mt-0">{{ $tweet->user->name }}</h5>
+                                    {{ $tweet->body }}
+                                </div>
+                            </div>
+                            @endforeach
+                        @endif
+                    @else
+                        <p>Please <a href="{{ url('/auth/passport') }}">authorize with Passport</a></p>
+                    @endif
                 </div>
             </div>
         </div>
