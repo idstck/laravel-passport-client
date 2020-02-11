9a74019d9220cbc03d00bf43a65596ee64144eb5
idstack <idstack@outlook.co.id>
2020-02-11 09:39:08 +0000

    Membuat Fungsi Callback dan Menyimpan Token

--- app/Http/Controllers/Auth/OauthController.php
+++ app/Http/Controllers/Auth/OauthController.php
@@ -4,9 +4,18 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use GuzzleHttp\Client as Guzzle;
 
 class OauthController extends Controller
 {
+    protected $client;
+
+    public function __construct(Guzzle $client)
+    {
+        $this->middleware('auth');
+        $this->client = $client;
+    }
+
     public function redirect()
     {
         $query = http_build_query([
@@ -19,8 +28,26 @@ class OauthController extends Controller
         return redirect('http://passport.test/oauth/authorize?' . $query);
     }
 
-    public function callback()
+    public function callback(Request $request)
     {
+        $response = $this->client->post('http://passport.test/oauth/token', [
+            'form_params' => [
+                'grant_type' => 'authorization_code',
+                'client_id' => '3',
+                'client_secret' => 'hPThwMipot4kQ18deI5sMko3Mt7cTHObyCahpeyV',
+                'redirect_uri' => 'http://passclient.test/auth/passport/callback',
+                'code' => $request->code,
+            ]
+        ]);
 
+        $response = json_decode($response->getBody());
+
+        $request->user()->token()->delete();
+
+        $request->user()->token()->create([
+            'access_token' => $response->access_token
+        ]);
+
+        return redirect('/home');
     }
 }
